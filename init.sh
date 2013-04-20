8340d4aed5af460d71db77d73bb603d7c4e035b6
johnkrovitch <arnaudfrezet@gmail.com>
2013-04-20 20:22:44 +0200

    updating initialization script (not fully functional yet), updating .gitignore

---
+++ init.sh
@@ -0,0 +1,22 @@
+# create uploads directory if required
+if [ ! -d "web/bundles/krovitch/uploads" ]; then
+  mkdir "web/bundles/krovitch/uploads"
+fi
+
+# create maps directory if required
+if [ ! -d "src/Krovitch/KrovitchBundle/Resources/maps" ]; then
+  mkdir "src/Krovitch/KrovitchBundle/Resources/maps"
+fi
+
+# set permissions for :
+#  - cache
+#  - logs
+#  - uploads directory
+#  - map data directory
+sudo setfacl -R -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs src/Krovitch/KrovitchBundle/Resources/maps
+
+# init composer
+php composer.phar update
+
+# bower
+bower install paper qunit jquery jquery.ui
--- init/init.sh
+++
@@ -1,9 +0,0 @@
-# init directories
-mkdir web/bundles/krovitch/uploads
-
-# set permissions for :
-#  - cache
-#  - logs
-#  - uploads directory
-#  - map data directory
-sudo setfacl -R -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs src/Krovitch/KrovitchBundle/Resources/maps
