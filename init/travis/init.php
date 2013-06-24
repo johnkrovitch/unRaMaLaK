a9d7303dbb8249b1fda02904c0a93611d964d595
johnkrovitch <arnaudfrezet@gmail.com>
2013-06-24 22:11:28 +0200

    updating travis init script

--- init/travis/init.php
+++ init/travis/init.php
@@ -5,6 +5,6 @@ $path = __DIR__.'/../../app/config';
 
 // to run php test, we should have a parameter.yml
 if (!is_file($path.'/parameters.yml')) {
-    copy($path.'/parameters-dist.yml', $path.'/parameters.yml');
+    copy($path.'/parameters.yml.dist', $path.'/parameters.yml');
     echo 'parameters.yml created !';
 }
