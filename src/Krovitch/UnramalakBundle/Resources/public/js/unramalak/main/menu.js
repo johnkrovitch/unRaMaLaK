ced55a9b4fe503eabc49bb8c176873f1fab01534
johnkrovitch <arnaudfrezet@gmail.com>
2013-12-07 22:10:00 +0100

    indentation

--- src/Krovitch/UnramalakBundle/Resources/public/js/unramalak/main/menu.js
+++ src/Krovitch/UnramalakBundle/Resources/public/js/unramalak/main/menu.js
@@ -1,69 +1,69 @@
 $.Class('Unramalak.Menu', {}, {
-  actions: null,
-  actionClicked: [],
-  container: null,
-  items: null,
-  data: [],
-  onSave: null,
+    actions: null,
+    actionClicked: [],
+    container: null,
+    items: null,
+    data: [],
+    onSave: null,
 
-  init: function (container) {
-    this.container = $(container);
-    this.items = this.container.find('.menu-item');
-    this.actions = this.container.find('.menu-action');
-  },
+    init: function (container) {
+        this.container = $(container);
+        this.items = this.container.find('.menu-item');
+        this.actions = this.container.find('.menu-action');
+    },
 
-  bind: function (onSave, map) {
-    var _super = this;
-    // click on actions buttons: call map callback
-    this.actions.on('click', function () {
-      if ($(this).data('action') == 'save') {
-        onSave.call(map||this);
-      }
-    });
-    // click on items buttons: save value
-    this.items.on('click', function (e) {
-      // if other items have been selected, we unselect them and select only current one
-      _super.unselect();
-      $(this).addClass('active');
-      // we save what item was pressed
-      _super.data[$(this).data('type')] = $(this).data('value');
-      // stop event propagation
-      e.stopPropagation();
-      e.preventDefault();
-    });
-  },
+    bind: function (onSave, map) {
+        var _super = this;
+        // click on actions buttons: call map callback
+        this.actions.on('click', function () {
+            if ($(this).data('action') == 'save') {
+                onSave.call(map || this);
+            }
+        });
+        // click on items buttons: save value
+        this.items.on('click', function (e) {
+            // if other items have been selected, we unselect them and select only current one
+            _super.unselect();
+            $(this).addClass('active');
+            // we save what item was pressed
+            _super.data[$(this).data('type')] = $(this).data('value');
+            // stop event propagation
+            e.stopPropagation();
+            e.preventDefault();
+        });
+    },
 
-  /**
-   * Return true if menu has data. Data are filled when user click on items buttons
-   * If type provided, return true if menu has data of this type (ex: land...)
-   * @param type {string}
-   * @returns {boolean}
-   */
-  hasData: function (type) {
-    return ($.isNull(type)) ? (this.data.length > 0) : this.data.hasOwnProperty(type);
-  },
+    /**
+     * Return true if menu has data. Data are filled when user click on items buttons
+     * If type provided, return true if menu has data of this type (ex: land...)
+     * @param type {string}
+     * @returns {boolean}
+     */
+    hasData: function (type) {
+        return ($.isNull(type)) ? (this.data.length > 0) : this.data.hasOwnProperty(type);
+    },
 
-  /**
-   * Return menu data. If type provided, return menu data of this type
-   * @param type {string}
-   * @returns {*}
-   */
-  getData: function (type) {
-    var data = this.data;
+    /**
+     * Return menu data. If type provided, return menu data of this type
+     * @param type {string}
+     * @returns {*}
+     */
+    getData: function (type) {
+        var data = this.data;
 
-    if ($.isNotNull(type)) {
-      data = data[type];
+        if ($.isNotNull(type)) {
+            data = data[type];
+        }
+        return data;
+    },
+
+    /**
+     * Unselect menu items
+     */
+    unselect: function () {
+        this.items.each(function () {
+            $(this).removeClass('active');
+        });
+        this.data = [];
     }
-    return data;
-  },
-
-  /**
-   * Unselect menu items
-   */
-  unselect: function () {
-    this.items.each(function () {
-      $(this).removeClass('active');
-    });
-    this.data = [];
-  }
 });
