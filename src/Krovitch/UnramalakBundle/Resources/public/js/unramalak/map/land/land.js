bb6c2c8efb19b8f3c8231c167e1ca003d96793a3
johnkrovitch <arnaudfrezet@gmail.com>
2014-01-27 23:02:36 +0100

    reindentation

--- src/Krovitch/UnramalakBundle/Resources/public/js/unramalak/map/land/land.js
+++ src/Krovitch/UnramalakBundle/Resources/public/js/unramalak/map/land/land.js
@@ -2,31 +2,31 @@
  * Unramalak.Land
  */
 $.Class('Unramalak.Land', {}, {
-  type: 'default',
-  image: null,
+    type: 'default',
+    image: null,
 
-  render: function () {
-    var render = {
-      type: 'default',
-      value: defaultBackgroundColor
-    };
-    // TODO put textures here
-    if (this.type == 'sand') {
-      render.type = 'image';
-      render.value = 'land_sand';
-    }
-    else if (this.type == 'water') {
-      render.type = 'image';
-      render.value = 'land_water';
-    }
-    else if (this.type == 'plains') {
-      render.type = 'image';
-      render.value = 'land_plains';
-    }
-    return render;
-  },
+    render: function () {
+        var render = {
+            type: 'default',
+            value: defaultBackgroundColor
+        };
+        // TODO put textures here
+        if (this.type == 'sand') {
+            render.type = 'image';
+            render.value = 'land_sand';
+        }
+        else if (this.type == 'water') {
+            render.type = 'image';
+            render.value = 'land_water';
+        }
+        else if (this.type == 'plains') {
+            render.type = 'image';
+            render.value = 'land_plains';
+        }
+        return render;
+    },
 
-  reset: function () {
-    this.type = 'default';
-  }
+    reset: function () {
+        this.type = 'default';
+    }
 });
