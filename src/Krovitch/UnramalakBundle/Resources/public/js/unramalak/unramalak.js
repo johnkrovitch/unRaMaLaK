31510cffd0f790f5970f149815908c687e47e743
johnkrovitch <arnaudfrezet@gmail.com>
2014-02-02 23:12:39 +0100

    indentation

--- src/Krovitch/UnramalakBundle/Resources/public/js/unramalak/unramalak.js
+++ src/Krovitch/UnramalakBundle/Resources/public/js/unramalak/unramalak.js
@@ -9,52 +9,52 @@
  * Main application
  */
 $.Class('Unramalak.Application', {}, {
-  editorContext: null,
-  editor: null,
-  mapContext: null,
-  map: null,
-  lastPointClicked: null,
-  renderer: null,
+    editorContext: null,
+    editor: null,
+    mapContext: null,
+    map: null,
+    lastPointClicked: null,
+    renderer: null,
 
-  init: function () {
-  },
+    init: function () {
+    },
 
-  /**
-   * Load a map
-   */
-  load: function (canvasId, mapData, textures) {
-    // get a reference to the canvas object
-    var canvas = document.getElementById(canvasId);
-    // create an empty project and a view for the canvas:
-    paper.setup(canvas);
+    /**
+     * Load a map
+     */
+    load: function (canvasId, mapData, textures) {
+        // get a reference to the canvas object
+        var canvas = document.getElementById(canvasId);
+        // create an empty project and a view for the canvas:
+        paper.setup(canvas);
 
-    // init map context
-    this.mapContext = new Unramalak.Map.Context({
-      cellPadding: 0,
-      data: mapData,
-      numberOfSides: 6,
-      mapContainer: canvasId,
-      menuContainer: '#editor-menu',
-      radius: 50,
-      preventBubbling: true,
-      startingPoint: new paper.Point(100, 50)
-    });
-    // load textures
-    Unramalak.ImageLoader.load(textures);
-  },
+        // init map context
+        this.mapContext = new Unramalak.Map.Context({
+            cellPadding: 0,
+            data: mapData,
+            numberOfSides: 6,
+            mapContainer: canvasId,
+            menuContainer: '#editor-menu',
+            radius: 50,
+            preventBubbling: true,
+            startingPoint: new paper.Point(100, 50)
+        });
+        // load textures
+        Unramalak.ImageLoader.load(textures);
+    },
 
-  /**
-   * Run application
-   */
-  run: function () {
-    // draw map and bind map's events
-    this.map = new Unramalak.Map.Map(this.mapContext);
-    this.map.build();
-    this.map.bind(this.notify);
-    this.map.render();
-  },
+    /**
+     * Run application
+     */
+    run: function () {
+        // draw map and bind map's events
+        this.map = new Unramalak.Map.Map(this.mapContext);
+        this.map.build();
+        this.map.bind(this.notify);
+        this.map.render();
+    },
 
-  notify: function (message, type) {
-    AlertManager.addAlert(message, type);
-  }
+    notify: function (message, type) {
+        AlertManager.addAlert(message, type);
+    }
 });
